c5b6af0f1baf3fbbd68f28d616263268748fe3fa
Karthik B M <karthikbm0808@gmail.com>
2025-08-09 12:24:32 +0530

    comment test cases

--- src/test/java/com/bank/oneBank/controller/UserControllerTest.java
+++ src/test/java/com/bank/oneBank/controller/UserControllerTest.java
@@ -8,26 +8,23 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 public class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private UserService userService;
-
-    @InjectMocks
-    private UserController userController;
-
-    @Test
-    public void createAccountTest(){
-
-
-
-    }
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Mock
+//    private UserService userService;
+//
+//    @InjectMocks
+//    private UserController userController;
+//
+//    @Test
+//    public void createAccountTest(){
+//
+//    }
 
 }
